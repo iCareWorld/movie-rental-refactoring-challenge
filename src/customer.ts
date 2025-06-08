fb80ddbe9bf82f89a1049dd4fbd4b3e90e509b21
Giulio <giuliomarcolin8@gmail.com>
2025-06-08 17:17:00 +0200

    refactor: cleaned up statement method using new pricing strategy

--- src/customer.ts
+++ src/customer.ts
@@ -2,56 +2,38 @@ import { Movie } from "./movie";
 import { Rental } from "./rental";
 
 export class Customer {
-    name: string;
-    rentals: Rental[];
+   
+    readonly rentals: Rental[] = [];
 
-    constructor(name: string) {
-        this.name = name
-        this.rentals = []
+    constructor(readonly name: string) { }
+
+    addRental(movie: Movie, daysRented: number): Rental {
+        const rental = new Rental(movie, daysRented);
+        this.rentals.push(rental);
+        return rental;
     }
 
-    addRental(movie: Movie, daysRented: number) {
-        let rental = new Rental(movie, daysRented)
-        this.rentals.push(rental)
-        return rental
+    getTotalAmount(): number {
+        return this.rentals.reduce((sum, rental) => sum + rental.getAmount(), 0);
+    }
+
+    getTotalFrequentRenterPoints(): number {
+        return this.rentals.reduce((sum, rental) => sum + rental.getFrequentRenterPoints(), 0);
     }
 
     statement() {
-        let totalAmount = 0
-        let frequentRenterPoints = 0
-        let result = "Rental record for " + this.name + "\n"
-        // determine amounts for each line
+        const totalAmount = this.getTotalAmount();
+        const frequentRenterPoints = this.getTotalFrequentRenterPoints();
 
-        for (const each of this.rentals) {
-            let thisAmount = 0
-            if (each.movie.priceCode.name === 'REGULAR') {
-                thisAmount += 2
-                if (each.daysRented > 2) {
-                    thisAmount += ((each.daysRented - 2) * 1.5)
-                }
-            }
-            else if (each.movie.priceCode.name === 'NEW RELEASE') {
-                thisAmount += each.daysRented * 3
-            }
-            else if (each.movie.priceCode.name === 'CHILDREN') {
-                thisAmount += 1.5;
-                if (each.daysRented > 3) {
-                    thisAmount = (each.daysRented - 3) * 1.5
-                }
-            }
-            // add frequent renter points
-            frequentRenterPoints++;
-            // add bonus for a two-day new-release rental
-            if ((each.movie.priceCode.name === 'NEW RELEASE') && (each.daysRented > 1)) {
-                frequentRenterPoints ++
-            }
-            // show figures for this rental
-            result += "\t" + each.movie.title + "\t" + thisAmount + "\n"
-            totalAmount += thisAmount
+        let result = `Rental record for ${this.name}\n`;
+
+        // determine amounts for each line
+        for (const rental of this.rentals) {
+            result += `\t${rental.movieTitle}\t${rental.getAmount()}\n`;
         }
         // add footer lines
-        result += "Amount owed is " + totalAmount + "\n"
-        result += "You earned " + frequentRenterPoints + " frequent renter points."
+        result += `Amount owed is ${totalAmount}\n`;
+        result += `You earned ${frequentRenterPoints} frequent renter points.`;
         return result
     }
 }
