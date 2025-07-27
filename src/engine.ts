c080c1007023983c40ca925e36f21905e942bd00
Paldeep Singh <paldeepsingh21@gmail.com>
2025-07-27 17:01:12 +1000

    Abstract customer to separate file

--- src/engine.ts
+++ src/engine.ts
@@ -1,42 +1,6 @@
 import { PriceCode } from "./constants/PriceCode";
+import { Customer } from "./entities/Customer";
 import { Movie } from "./entities/Movie";
-import { Rental } from "./entities/Rental";
-
-class Customer {
-  name: string;
-  rentals: Rental[];
-
-  constructor(name: string) {
-    this.name = name;
-    this.rentals = [];
-  }
-
-  addRental(movie: Movie, daysRented: number) {
-    let rental = new Rental(movie, daysRented);
-    this.rentals.push(rental);
-    return rental;
-  }
-
-  statement() {
-    let totalAmount = 0;
-    let frequentRenterPoints = 0;
-    let result = "Rental record for " + this.name + "\n";
-    // determine amounts for each line
-
-    for (const each of this.rentals) {
-      // add frequent renter points
-      frequentRenterPoints = frequentRenterPoints + each.pointsEarned;
-      // show figures for this rental
-      result += "\t" + each.movie.title + "\t" + each.price + "\n";
-
-      totalAmount += each.price;
-    }
-    // add footer lines
-    result += "Amount owed is " + totalAmount + "\n";
-    result += "You earned " + frequentRenterPoints + " frequent renter points.";
-    return result;
-  }
-}
 
 export class Store {
   movies: Movie[];
