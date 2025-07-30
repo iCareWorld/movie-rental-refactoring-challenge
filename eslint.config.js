3cf6239f5270194a17b55b69c587477abde57873
Moditha Akalanka <Moditha.Hengodage@idomi.com.au>
2025-07-30 21:55:09 +1000

    chore: add ESLint configuration and dependencies
      - Create eslint.config.js for TypeScript linting rules
      - Add ESLint and TypeScript ESLint plugin dependencies
      - Update package.json with linting scripts
    
    eslint.config.js, package.json, package-lock.json

---
+++ eslint.config.js
@@ -0,0 +1,57 @@
+const js = require('@eslint/js')
+
+module.exports = [
+    js.configs.recommended,
+    {
+        files: ['src/**/*.ts'],
+        languageOptions: {
+            parser: require('@typescript-eslint/parser'),
+            parserOptions: {
+                ecmaVersion: 2022,
+                sourceType: 'module',
+            },
+            globals: {
+                console: 'readonly',
+                process: 'readonly',
+                module: 'readonly',
+                require: 'readonly',
+                exports: 'readonly',
+                __dirname: 'readonly',
+                __filename: 'readonly',
+                global: 'readonly',
+                Buffer: 'readonly',
+            },
+        },
+        plugins: {
+            '@typescript-eslint': require('@typescript-eslint/eslint-plugin'),
+        },
+        rules: {
+            'no-unused-vars': 'off',
+            '@typescript-eslint/no-unused-vars': 'error',
+            'no-console': 'off',
+            'prefer-const': 'error',
+            'no-var': 'error',
+            'eqeqeq': 'error',
+            'curly': 'error',
+            'brace-style': ['error', '1tbs'],
+            'indent': ['error', 4],
+            'quotes': ['error', 'single'],
+            'semi': ['error', 'never'],
+        },
+    },
+    {
+        files: ['src/**/*.test.ts'],
+        languageOptions: {
+            globals: {
+                describe: 'readonly',
+                it: 'readonly',
+                expect: 'readonly',
+                beforeEach: 'readonly',
+                afterEach: 'readonly',
+                beforeAll: 'readonly',
+                afterAll: 'readonly',
+                jest: 'readonly',
+            },
+        },
+    },
+]
