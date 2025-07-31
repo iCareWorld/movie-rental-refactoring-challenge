0d04cb85d0062836f9b9d674e4ad749217079424
Darkfish Tech <github@darkfish.com.au>
2025-07-31 23:03:07 +1000

    Add behaviours to Store to act as manager
    Store object now manages movies, customers and rentals.
    
    However, statement generation of statement is currently partially done
    by a service object.

--- src/engine.ts
+++ src/engine.ts
@@ -1,81 +1,18 @@
-class PriceCode {
-    constructor(public readonly name: string) {}
-}
-
-class Movie {
-    constructor(public readonly title: string, public readonly priceCode: PriceCode) {}
-}
-
-class Rental {
-    constructor(public readonly movie: Movie, public readonly daysRented: number) {}
-}
-
-class Customer {
-    name: string;
-    rentals: Rental[];
-
-    constructor(name: string) {
-        this.name = name
-        this.rentals = []
-    }
-
-    addRental(movie: Movie, daysRented: number) {
-        let rental = new Rental(movie, daysRented)
-        this.rentals.push(rental)
-        return rental
-    }
-
-    statement() {
-        let totalAmount = 0
-        let frequentRenterPoints = 0
-        let result = "Rental record for " + this.name + "\n"
-        // determine amounts for each line
-
-        for (const each of this.rentals) {
-            let thisAmount = 0
-            if (each.movie.priceCode.name === 'REGULAR') {
-                thisAmount += 2
-                if (each.daysRented > 2) {
-                    thisAmount += ((each.daysRented - 2) * 1.5)
-                }
-            }
-            else if (each.movie.priceCode.name === 'NEW RELEASE') {
-                thisAmount += each.daysRented * 3
-            }
-            else if (each.movie.priceCode.name === 'CHILDREN') {
-                thisAmount += 1.5;
-                if (each.daysRented > 3) {
-                    thisAmount = (each.daysRented - 3) * 1.5
-                }
-            }
-            // add frequent renter points
-            frequentRenterPoints++;
-            // add bonus for a two-day new-release rental
-            if ((each.movie.priceCode.name === 'NEW RELEASE') && (each.daysRented > 1)) {
-                frequentRenterPoints ++
-            }
-            // show figures for this rental
-            result += "\t" + each.movie.title + "\t" + thisAmount + "\n"
-            totalAmount += thisAmount
-        }
-        // add footer lines
-        result += "Amount owed is " + totalAmount + "\n"
-        result += "You earned " + frequentRenterPoints + " frequent renter points."
-        return result
-    }
-}
+import {Customer} from "./models/customer";
+import {Movie} from './models/movie'
+import {PriceCode} from './models/price_code'
+import {Rental} from "./models/rental";
+import {generateStatement} from "./services/statement_generator";
 
 export class Store {
-    static readonly PRICE_CODE_REGULAR: PriceCode = new PriceCode('REGULAR')
-    static readonly PRICE_CODE_CHILDREN: PriceCode = new PriceCode('CHILDREN')
-    static readonly PRICE_CODE_NEW_RELEASE: PriceCode = new PriceCode('NEW RELEASE')
-
-    movies: Movie[]
-    customers: Customer[]
+    private movies: Movie[]
+    private customers: Customer[]
+    private rentals: Rental[]
 
     constructor() {
         this.movies = []
         this.customers = []
+        this.rentals = []
     }
 
     addMovie(name: string, priceCode: PriceCode) {
@@ -88,5 +25,35 @@ export class Store {
         let customer = new Customer(name)
         this.customers.push(customer)
         return customer
+    }
+
+    addRental(customer: Customer, movie: Movie, daysRented: number) {
+        let rental = new Rental(customer, movie, daysRented)
+        this.rentals.push(rental)
+        return rental
+    }
+
+    getMovies(): Movie[] {
+        return this.movies.slice()
+    }
+
+    getCustomers(): Customer[] {
+        return this.customers.slice()
+    }
+
+    getRentals(): Rental[] {
+        return this.rentals.slice()
+    }
+
+    findRentalsForCustomer(customer: Customer): Rental[] {
+        return this.rentals.filter(rental => rental.customer === customer)
+    }
+
+    getStatementForCustomer(customer: Customer): string {
+        const rentals = this.findRentalsForCustomer(customer)
+        if (rentals.length === 0) {
+            return `No rentals found for customer: ${customer.name}`
+        }
+        return generateStatement(rentals, customer)
     }
 }
